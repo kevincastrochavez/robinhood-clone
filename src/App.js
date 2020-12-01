4f0a281596e02de8f5d228339808a1f0151543e6
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-11-30 22:21:34 -0600

    set the layout for the newsfeed and the stats component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-const { default: Header } = require("./components/Header");
+import Header from "./components/Header";
+import NewsFeed from "./components/NewsFeed";
+import Stats from "./components/Stats";
 
 function App() {
   return (
     <div className="app">
       <Header className="app__header" />
+
+      <div className="app__body">
+        <div className="app__body-container">
+          <NewsFeed />
+
+          <Stats />
+        </div>
+      </div>
     </div>
   );
 }
