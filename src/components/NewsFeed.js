cb5ec50e3e8fecde80179066d2acee5839791287
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-12-02 23:53:51 -0600

    installed chart.js and react-chartjs-2, imported the line component and gave it some styles to display a sample chart

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,9 +1,20 @@
 import React from "react";
+import LineGraph from "./LineGraph";
 
 function NewsFeed() {
   return (
     <div className="newsFeed">
-      <div className="newsFeed__container"></div>
+      <div className="newsFeed__container">
+        <div className="newsFeed__container-chart">
+          <div className="newsFeed__container-chart-portfolio">
+            <h1>$114,656.84</h1>
+            <p>+$44.63 (+0.04%) Today</p>
+          </div>
+          <div className="newsFeed__container-chart-chart">
+            <LineGraph />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
